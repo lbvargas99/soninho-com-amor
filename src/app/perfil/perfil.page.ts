efa87963f0ca5f669c17f4cc1e0485a268474ade
Leonardo Borges <lbvargas99@gmail.com>
2023-09-20 15:26:33 -0300

    implemented form with data

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Usuario } from '../models/Usuario';
+import { FormDataService } from '../services/form-data.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.page.html',
-  styleUrls: ['./perfil.page.scss'],
+	selector: 'app-perfil',
+	templateUrl: './perfil.page.html',
+	styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+	usuario!: Usuario;
+	perfilForm!: FormGroup;
 
-  ngOnInit() {
-  }
+	
+
+	constructor(
+		private fb: FormBuilder,
+		private formDataService: FormDataService,
+		private router: Router) { }
+
+	ngOnInit() {
+		this.usuario = this.formDataService.getLoggerUser();
+
+		this.perfilForm = this.fb.group({
+			nome: [this.usuario.nome],
+			sobrenome: [this.usuario.sobrenome],
+			email: [this.usuario.email],
+			senha: [this.usuario.senha]
+		});
+	}
+
+	onSubmit() {
+		const formData: Usuario = {
+			nome: this.perfilForm.value?.nome || '',
+			sobrenome: this.perfilForm.value.sobrenome || '',
+			email: this.perfilForm.value.email || '',
+			senha: this.perfilForm.value.senha || ''
+		}
+
+		this.formDataService.updateUser(this.usuario.email, formData)
+		console.log(this.formDataService.getLoggerUser())
+		this.router.navigate(['/inicio-conta']);
+	}
 
 }
