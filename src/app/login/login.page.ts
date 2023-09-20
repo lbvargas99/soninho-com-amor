0867f16737a8ec886cb95064327deaf80d0193eb
Leonardo Borges <lbvargas99@gmail.com>
2023-09-20 15:26:57 -0300

    implemented form with data

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormDataService } from '../services/form-data.service';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
-  backIcon: string = '<';
-  constructor() { }
+export class LoginPage {
+  loginForm = this.fb.group({
+    email: [''],
+    senha: ['']
+  });
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private formDataService: FormDataService,
+    private router: Router) { }
+
+  onSubmit() {
+    const email = this.loginForm.get('email')?.value;
+    const senha = this.loginForm.get('senha')?.value;
+
+    const usuario = this.formDataService
+      .getUsuarios()
+      .find((user) => user.email === email && user.senha === senha
+      );
+
+    if (usuario) {
+      this.formDataService.setLoggedUser(usuario);
+      
+      this.router.navigate(['/inicio-conta'])
+    } else {
+      alert('E-mail ou senha inválida')
+    }
   }
 
 }
