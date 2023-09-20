b2cf9eb073e164261b9db714c0206572758b12a2
Leonardo Borges <lbvargas99@gmail.com>
2023-09-20 15:26:12 -0300

    create new methods

--- src/app/services/form-data.service.ts
+++ src/app/services/form-data.service.ts
@@ -15,11 +15,30 @@ export class FormDataService {
     }
   ];
 
+  private userLogged: Usuario = new Usuario('', '', '', '', );
+
   addUsuario(usuario: Usuario) {
     this.usuarios.push(usuario);
   }
 
   getUsuarios() {
     return this.usuarios;
+  }
+
+  getLoggerUser() {
+    return this.userLogged;
+  }
+
+  setLoggedUser(usuario: Usuario) {
+    this.userLogged = usuario;
+  }
+
+  updateUser(oldUserEmail: string, newUser: Usuario) {
+    const index = this.usuarios.findIndex((user) => user.email === oldUserEmail);
+
+    if (index !== -1) {
+      this.usuarios[index] = newUser;
+      this.userLogged = newUser
+    }
   }
 }
